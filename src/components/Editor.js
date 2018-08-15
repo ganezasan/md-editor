6f79b31bf1d78ff9cf474b8188a21012ec80a7cc
ganezasan <istaka12@gmail.com>
2018-08-15 13:10:13 +0900

    add hint

--- src/components/Editor.js
+++ src/components/Editor.js
@@ -1,24 +1,54 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { UnControlled as CodeMirror } from 'react-codemirror2';
 
-require('codemirror/mode/markdown/markdown');
+require('codemirror/addon/hint/show-hint');
+require('codemirror/addon/hint/show-hint.css');
+require('codemirror/mode/markdown/markdown'); // without this css hints won't show
 
-const Editor = ({ src, onChangeSrc }) => (
-  <CodeMirror
-    value={src}
-    options={{
+const emojiList = ['apple:', ':abc:', 'axz:', 'bee:', 'beam:', 'bleach:'];
+class Editor extends Component {
+  handleKeyUpEvent(editor, target) {
+    if (target.key === '@') {
+      const options = {
+        hint() {
+          return {
+            from: editor.getDoc().getCursor(),
+            to: editor.getDoc().getCursor(),
+            list: emojiList,
+          };
+        },
+      };
+      editor.showHint(options);
+    }
+  }
+
+  render() {
+    const { src, onChangeSrc } = this.props;
+    const options = {
       mode: 'markdown',
       theme: 'material',
       lineNumbers: true,
-    }}
-    className="editor-pane"
-    height="100%"
-    onChange={(editor, data, value) => {
-      onChangeSrc(value);
-    }}
-  />
-);
+      lineWrapping: true,
+      completeSingle: false,
+      completeOnSingleClick: false,
+    };
+
+    return (
+      <CodeMirror
+        ref="CodeMirror"
+        value={src}
+        options={options}
+        className="editor-pane"
+        height="100%"
+        onKeyDown={this.handleKeyUpEvent}
+        onChange={(editor, target, value) => {
+          onChangeSrc(value);
+        }}
+      />
+    );
+  }
+}
 
 Editor.propTypes = {
   src: PropTypes.string,
