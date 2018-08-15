f0354a5e76bc27d5eef158d10f800285eeb1211a
ganezasan <istaka12@gmail.com>
2018-08-15 23:29:57 +0900

    feat(Editor): 絵文字とユーザのヒントを表示する機能を追加

--- src/components/Editor.js
+++ src/components/Editor.js
@@ -6,19 +6,57 @@ require('codemirror/addon/hint/show-hint');
 require('codemirror/addon/hint/show-hint.css');
 require('codemirror/mode/markdown/markdown'); // without this css hints won't show
 
-const emojiList = ['apple:', ':abc:', 'axz:', 'bee:', 'beam:', 'bleach:'];
+const hintRender = (elt, data, cur) => {
+  const url =
+    'https://assets-cdn.github.com/images/icons/emoji/unicode/1f44d.png?v8';
+  const displayNode = `<img width="15" height="15" src="${url}" alt="icon" async></img> ${
+    cur.text
+  }`;
+  elt.innerHTML = displayNode;
+  elt.hintId = cur.i;
+  return elt;
+};
+
+const emojiList = ['apple', 'abc', 'axz', 'bee', 'beam', 'bleach'].map(
+  (key, i) => ({
+    text: `${key}:`,
+    className: 'emoji',
+    i,
+    render: hintRender,
+  }),
+);
+
+const mentionList = [
+  'taka',
+  'koyo',
+  'kato',
+  'tanaka',
+  'tachibana',
+  'takashi',
+].map((key, i) => ({
+  text: key,
+  className: 'mention',
+  i,
+  render: hintRender,
+}));
+
+const getHint = (editor, list) => ({
+  hint: () => ({
+    from: editor.getDoc().getCursor(),
+    to: editor.getDoc().getCursor(),
+    list,
+  }),
+  closeCharacters: /[\s()\[\]{};>,]/,
+  completeSingle: false,
+});
+
 class Editor extends Component {
-  handleKeyUpEvent(editor, target) {
+  handleKeyDown(editor, target) {
     if (target.key === '@') {
-      const options = {
-        hint() {
-          return {
-            from: editor.getDoc().getCursor(),
-            to: editor.getDoc().getCursor(),
-            list: emojiList,
-          };
-        },
-      };
+      const options = getHint(editor, mentionList);
+      editor.showHint(options);
+    } else if (target.key === ':') {
+      const options = getHint(editor, emojiList);
       editor.showHint(options);
     }
   }
@@ -41,7 +79,7 @@ class Editor extends Component {
         options={options}
         className="editor-pane"
         height="100%"
-        onKeyDown={this.handleKeyUpEvent}
+        onKeyDown={this.handleKeyDown}
         onChange={(editor, target, value) => {
           onChangeSrc(value);
         }}
