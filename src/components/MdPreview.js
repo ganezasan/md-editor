dff43aa186c55e5cca7bc97aa2bed971c3cf6808
ganezasan <istaka12@gmail.com>
2018-08-18 23:08:39 +0900

    emojiListを取得、一旦Unicodeでemojiを表示

--- src/components/MdPreview.js
+++ src/components/MdPreview.js
@@ -1,10 +1,32 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import ReactMarkdown from 'react-markdown';
+import emoji from 'emoji-dictionary';
+import { createStructuredSelector } from 'reselect';
+import { connect } from 'react-redux';
+import { getList } from '../redux/emoji/selectors';
 
-const MdPreview = ({ src }) => (
+const makeImage = (name, url) => (
+  <img width="15" height="15" src="${url}" alt="${name}" async />
+);
+
+const emojiSupport = (text, emojiList) =>
+  text.replace(
+    /:\w+:/gi,
+    name => emoji.getUnicode(name),
+    // const key = name.replace(/:/g, '');
+    // TODO 画像にならない
+    // const emojiUrl = emojiList[key];
+    // return emojiUrl ? makeImage(key, emojiUrl) : name;
+  );
+
+const MdPreview = ({ src, emojiList }) => (
+  // console.log(emojiList);
   <div className="md-preview">
-    <ReactMarkdown source={src} />
+    <ReactMarkdown
+      source={src}
+      renderers={{ text: text => emojiSupport(text, emojiList) }}
+    />
   </div>
 );
 
@@ -12,4 +34,11 @@ MdPreview.propTypes = {
   src: PropTypes.string,
 };
 
-export default MdPreview;
+const mapStateToProps = createStructuredSelector({
+  emojiList: getList,
+});
+
+export default connect(
+  mapStateToProps,
+  null,
+)(MdPreview);
