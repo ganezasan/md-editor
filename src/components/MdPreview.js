230e3529aac06fca50432b3fc656f057236e2bb8
ganezasan <istaka12@gmail.com>
2018-08-19 17:25:45 +0900

    feat(Preview): 絵文字のプレビューに対応

--- src/components/MdPreview.js
+++ src/components/MdPreview.js
@@ -1,32 +1,28 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import ReactMarkdown from 'react-markdown';
-import emoji from 'emoji-dictionary';
+import remark from 'remark';
+import reactRenderer from 'remark-react';
+import textToImage from 'remark-text-to-image';
+import toc from 'remark-toc';
+
 import { createStructuredSelector } from 'reselect';
 import { connect } from 'react-redux';
 import { getList } from '../redux/emoji/selectors';
 
-const makeImage = (name, url) => (
-  <img width="15" height="15" src="${url}" alt="${name}" async />
-);
-
-const emojiSupport = (text, emojiList) =>
-  text.replace(
-    /:\w+:/gi,
-    name => emoji.getUnicode(name),
-    // const key = name.replace(/:/g, '');
-    // TODO 画像にならない
-    // const emojiUrl = emojiList[key];
-    // return emojiUrl ? makeImage(key, emojiUrl) : name;
-  );
-
 const MdPreview = ({ src, emojiList }) => (
-  // console.log(emojiList);
   <div className="md-preview">
-    <ReactMarkdown
-      source={src}
-      renderers={{ text: text => emojiSupport(text, emojiList) }}
-    />
+    {
+      remark()
+        .use(toc)
+        .use(textToImage, {
+          base: 'https://assets-cdn.github.com/images/icons/emoji/unicode/',
+          list: emojiList, // [key: unicode]
+        })
+        .use(reactRenderer, {
+          sanitize: false,
+        })
+        .processSync(src).contents
+    }
   </div>
 );
 
